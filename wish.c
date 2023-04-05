ff148a862c35cb603016e89237cd72c7b9354297
Hanbin Lee <hve4638@gmail.com>
2023-04-05 10:38:07 +0900

    add wish.c

---
+++ wish.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    const char** buffer;
+    int capacity = 0;
+    int length;
+
+    while(1) {
+        length = getline(buffer, &capacity, stdin);
+
+        
+    }
+}
