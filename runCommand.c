797c2148f407b6db1a4557be45b48e5b241c9b0a
Kangmin Bea <bkm0096@gmail.com>
2023-04-10 21:11:26 +0900

    upload

--- runCommand.c
+++ runCommand.c
@@ -2,58 +2,68 @@
 #include <sys/wait.h>
 #include "wish.h"
 
-void runCommand(CommandNode* commands) {
+void runCommand(CommandNode *commands)
+{
     pid_t pid;
     int status;
-    while (commands != NULL) {
-        Command* cmd = commands->command;
+    while (commands != NULL)
+    {
+        Command *cmd = commands->command;
 
-        if (isBuiltInCommand(cmd)) { // 내장 프로그램일 경우
+        if (isBuiltInCommand(cmd))
+        { // 내장 프로그램일 경우
             runBuiltInCommand(cmd);
-        } else { // 외부 프로그램일 경우
-            const char** p = wishPATH;
+        }
+        else
+        { // 외부 프로그램일 경우
+            const char **p = wishPATH;
+            char path[4096];
 
-            while (*p != '\0') {
-                /*
-                    정확히 어떻게 동작하는지 모르겠는데 p는 프로그램이 아니라 디렉토리
-                    디렉토리의 실행권한을 체크해야 하는지? 잘모르겠음
-                    아마 디렉토리의 읽기권한 받아오는건 필요할 수도?
-                */
-                if (access(p, X_OK) == 0) { //X_OK: 파일의 실행 권한 여부를 확인
-                    break;
+            while (*p != NULL)
+            {
+                snprintf(path, sizeof(path), "%s/%s", *p, cmd->args[0]);
+                if (access(path, X_OK) == 0){   // 디렉토리의 실행 권한 여부를 확인
+                break;
                 }
-                p++;
+            p++;
             }
-
-            if (p == NULL) { //경로에서 명령어를 찾지 못했을 경우
-            /*
-                프로그램 요구사항에서 error()만 호출하면 될듯?
-            */
-                fprintf(stderr, "wish: %s: command not found\n", cmd->cmd);
+            if (*p == NULL)
+            { // 경로에서 명령어를 찾지 못했을 경우
+                char error_message[30] = "An error has occurred\n";
+                write(STDERR_FILENO, error_message, strlen(error_message));
                 commands = commands->next;
                 continue;
             }
 
             pid = fork();
 
-            if (pid == 0) {
+            if (pid == 0)
+            {
                 // 자식프로세서
-                if (cmd->redirectTo != NULL) {
-                    FILE* outFile = fopen(cmd->redirectTo, "w");
-                    if (outFile == NULL) { //파일 열기 실패
+                if (cmd->redirectTo != NULL)
+                {
+                    FILE *outFile = fopen(cmd->redirectTo, "w");
+                    if (outFile == NULL)
+                    { // 파일 열기 실패
+                        char error_message[30] = "An error has occurred\n";
+                        write(STDERR_FILENO, error_message, strlen(error_message));
                         exit(1);
                     }
                     dup2(fileno(outFile), STDOUT_FILENO); // 이후의 printf와 같은 출력은 모두 outFile에 저장
                 }
 
-                execv(p, (char* const*) cmd->args); // args의 마지막은 NULL로 끝나야함
+                execv(path, (char *const *)cmd->args); // args의 마지막은 NULL로 끝나야함
                 perror("execv failed");
                 exit(1);
-            } else if (pid < 0) { //fork() 호출 실패할 경우
+            }
+            else if (pid < 0)
+            { // fork() 호출 실패할 경우
+                char error_message[30] = "An error has occurred\n";
+                write(STDERR_FILENO, error_message, strlen(error_message));
                 exit(1);
             }
             // 부모 프로세서
-            if (waitpid(pid, &status, 0) == -1) { 
+            if (waitpid(pid, &status, 0) == -1){
                 exit(1);
             }
         }
